aa450c0293775f2995a7ead6fdf31723ba050d5f
Tom Brandon <tom.j.v.brandon@gmail.com>
2018-02-07 18:24:26 +0000

    Can now add models to the database. Can retrieve models and display them using the react app

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
+//routes
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+//express
 var app = express();
+
+//development only
+mongoose.connect('mongodb://localhost/test2');
+// mongoose.model('users', {name: String});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +30,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', index);
 app.use('/users', users);
 
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var User = require('../models/users')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  // res.send('respond with a resource');
-  const team = [{ id: 1, name: "Varun"},
-  {id: 2, name: "Tom"},
-  {id: 3, name: "Charles"},
-  {id: 4, name: "Umair"}];
 
-  res.json(team);
+  // var user = new User({ name: "Harry" });
+  // user.save();
+
+  // var user = User.findOne({name: "Tom"})
+    // .then(res.send(user));
+
+
+  User.find(function(err, users){
+      res.send(users);
+  });
+
+
+  // res.send('respond with a resource');
+  // const team = [{ id: 1, name: "Varun"},
+  // {id: 2, name: "Tom"},
+  // {id: 3, name: "Charles"},
+  // {id: 4, name: "Umair"}];
+  //
+  // res.json(team);
 });
 
 module.exports = router;
