1f061dc9545f549429adea55814128eebbf2fd99
Tom Brandon <tom.j.v.brandon@gmail.com>
2018-02-08 16:38:31 +0000

    Installed jest-fetch-mock

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const wrapper = shallow(<App />)
+
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+describe ('testing App component', () => {
+  let wrapper;
+  beforeEach(() => { wrapper = shallow(<App />); });
+
+  it('App renders as a div', () => {
+    expect(wrapper.type()).toBe('div');
+  });
+
 });
